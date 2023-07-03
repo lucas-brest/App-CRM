2d7254efaa8ff4fec71e95419e333cd08773378e
Lucas Brest <lucas.brest@moxit.com.ar>
2023-07-03 13:19:39 -0300

    Add link box index

--- Migrations/20230703161147_edituserkey.Designer.cs
+++ Migrations/20230703161147_edituserkey.Designer.cs
@@ -4,6 +4,7 @@ using App_CRM.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace App_CRM.Migrations
 {
     [DbContext(typeof(CRMDBContext))]
-    partial class CRMDBContextModelSnapshot : ModelSnapshot
+    [Migration("20230703161147_edituserkey")]
+    partial class edituserkey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Model/Usuario.cs
+++ Model/Usuario.cs
@@ -4,10 +4,9 @@ namespace App_CRM.Model
 {
 	public class Usuario
 	{
-		[Key]
-		public int IdUsuario { get; set; }
         public bool IsAdminRol { get; set; }
         public string? Nombre { get; set; }
+		[Key]
         public string? Username { get; set; }
         public string? Password { get; set; }
         public bool Estado { get; set; }
