6b5f9c90e307f4aff0fda594255647412264cc47
Lucas Brest <lucas.brest@moxit.com.ar>
2023-07-06 10:43:45 -0300

    Change usuario

--- Model/Usuario.cs
+++ Model/Usuario.cs
@@ -4,13 +4,11 @@ namespace App_CRM.Model
 {
 	public class Usuario
 	{
-        public bool IsAdminRol { get; set; }
-        public string? Nombre { get; set; }
 		[Key]
-        public string? Username { get; set; }
+        public string Username { get; set; }
+        public string? Nombre { get; set; }
         public string? Password { get; set; }
-        public bool Estado { get; set; }
-        public DateTime? UltimoLogin { get; set; }
+        public bool IsAdminRol { get; set; }
 
     }
 }
